8a83d934aa11aa1deda1d0b359453ea61ae49a16
KentY <e6ony.ivory@gmail.com>
2023-10-15 18:04:16 +0900

    ホーム画面作成

--- header.php
+++ header.php
@@ -1,1 +1,27 @@
-<header>ヘッダー</header>
+<header class="h-20">
+        <div class="d-flex justify-content-between container-fluid bg-dark">
+            <!-- タイトル -->
+            <div class="h3 m-4 text-danger">HOME LIFE</div>
+            <!-- ナビゲーション -->
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <ul class="navbar-nav">
+                      <li class="nav-item m-2">
+                        <a class="nav-link active" href="#">Home</a>
+                      </li>
+                      <li class="nav-item m-2">
+                        <a class="nav-link" href="#">About</a>
+                      </li>
+                      <li class="nav-item m-2">
+                        <a class="nav-link" href="#">Case</a>
+                      </li>
+                      <li class="nav-item m-2">
+                        <a class="nav-link" href="#">Service</a>
+                      </li>
+                      <li class="nav-item m-2">
+                        <a class="nav-link" href="#">FAQ</a>
+                      </li>
+                    </ul>
+                  </nav>
+            </div>
+</header>
--- home.php
+++ home.php
@@ -1,1 +1,13 @@
-<div>home</div>
+<div class="position-relative">
+    <img class="img-fluid" src="./assets/images/home.jpg" alt="">
+    <div class="d-flex position-absolute top-0 start-0 w-100 h-100 justify-content-center align-items-center">
+        <div class="d-flex flex-column bg-light w-50 h-50 rounded justify-content-center align-items-center" style="--bs-bg-opacity: .7">
+            <div class="h1 p-2 border-bottom border-dark">
+                夢の住まいを現実に
+            </div>
+            <div class="m-4 fs-5 fw-bold">
+                私たちはお客様の希望やビジョンに真摯に向き合い、<br>最適な不動産ソリューションを提供します
+            </div>
+        </div>
+    </div>
+</div>
