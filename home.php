f223cbb9f8494d2e4bab87bfc1201b5a1ea4d06f
KentY <e6ony.ivory@gmail.com>
2023-10-17 23:46:27 +0900

    css微調整

--- home.php
+++ home.php
@@ -2,8 +2,8 @@
     <div class="row">
         <img class="img-fluid p-0" src="./assets/images/home.jpg" alt="">
         <div class="d-flex position-absolute top-0 start-0 h-100 overflow-auto justify-content-center align-items-center">
-            <div class="col-md-5 d-flex flex-column bg-light mx-auto my-auto py-5 rounded justify-content-center align-items-center" style="--bs-bg-opacity: .7">
-                <div class="h1 p-2 border-bottom border-dark">
+            <div class="col-md-5 d-flex flex-column bg-light mx-auto my-auto py-4 rounded justify-content-center align-items-center" style="--bs-bg-opacity: .7">
+                <div class="h2 p-2 border-bottom border-dark">
                     夢の住まいを現実に
                 </div>
                 <div class="m-4 h5">
