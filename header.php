945f89323e983de73aab7eb6369663fd2cff09c9
KentY <e6ony.ivory@gmail.com>
2023-10-22 12:34:56 +0900

    微修正

--- header.php
+++ header.php
@@ -52,27 +52,5 @@
                   </button>
                 </div>
               </nav>
-                <!-- <nav class="navbar navbar-expand-lg navbar-dark">
-                    <ul class="navbar-nav">
-                      <li class="nav-item m-2">
-                        <a class="nav-link active" href="#">Home</a>
-                      </li>
-                      <li class="nav-item m-2">
-                        <a class="nav-link" href="#about">About</a>
-                      </li>
-                      <li class="nav-item m-2">
-                        <a class="nav-link" href="#case">Case</a>
-                      </li>
-                      <li class="nav-item m-2">
-                        <a class="nav-link" href="#service">Service</a>
-                      </li>
-                      <li class="nav-item m-2">
-                        <a class="nav-link" href="#faq">FAQ</a>
-                      </li>
-                      <li class="nav-item m-2">
-                        <a class="nav-link" href="#contactUs">Contact Us</a>
-                      </li>
-                    </ul>
-                  </nav> -->
             </div>
 </header>
